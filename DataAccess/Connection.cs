a9dc5da98c1e71bad4131e4e9f7d935bedaa0b09
Johan Steven Ramirez Murcia <johan05182002.com@gmail.com>
2025-03-31 15:50:09 -0500

    Fix (Errores varios)
    productos, catalogo, negocio
    
    aun queda algo en el registo de usuario

--- DataAccess/Connection.cs
+++ DataAccess/Connection.cs
@@ -12,7 +12,9 @@ namespace DataAccess
         {
 
 
-            string cadenaConnection = "server=localhost; database=abastecete; user=root; password=smati0422; port=3306";
+            //string cadenaConnection = "server=localhost; database=abastecete; user=root; password=0518; port=3306";
+
+            string cadenaConnection = "server=167.71.91.199; database=abastecete; user=bd_abastecete; password=root_abastecete; port=3306";
 
             connection = new MySqlConnection(cadenaConnection);
             try
