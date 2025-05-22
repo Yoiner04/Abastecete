249c136eea22cbbfe8d085e0ff4b0e66b37181dd
Johan Steven Ramirez Murcia <johan05182002.com@gmail.com>
2025-05-21 21:19:01 -0500

    Modificacion vistas responsive

--- BusinessLogic/ManejadorMongo.cs
+++ BusinessLogic/ManejadorMongo.cs
@@ -13,7 +13,8 @@ namespace BusinessLogic
         private readonly GridFSBucket _gridFS;
         public ManejadorMongo()
         {
-            var cliente = new MongoClient("mongodb+srv://websencol:%40WenSEN.Col_2024@websen.kgr8b.mongodb.net/WebSEN?retryWrites=true&w=majority");
+            var cliente = new MongoClient("mongodb://localhost:27017/");
+            //var cliente = new MongoClient("mongodb+srv://websencol:%40WenSEN.Col_2024@websen.kgr8b.mongodb.net/WebSEN?retryWrites=true&w=majority");
             _db = cliente.GetDatabase("abastecete");
             _gridFS = new GridFSBucket(_db);
         }
