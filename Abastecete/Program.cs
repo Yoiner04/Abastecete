50509b68951777140e935ac817cc9255d833abde
Johan Steven Ramirez Murcia <johan05182002.com@gmail.com>
2025-05-31 10:29:00 -0500

    fix :(program)
    Se cambio una validacion para produccion

--- Abastecete/Program.cs
+++ Abastecete/Program.cs
@@ -43,13 +43,6 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Middleware
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
